c090a4f419f6eda1f9b3798859d7dc5008726f82
Franck Decaux <franck.decaux@ac-amiens.fr>
2025-05-22 18:30:41 +0200

    Update memoisation.py

--- memoisation/memoisation.py
+++ memoisation/memoisation.py
@@ -23,6 +23,24 @@ def nm(m, n, memo=None):
     return memo[(m, n)]
 
 
+# avec "liste de listes" de mémoïsation passé en paramètre de la fonction récursive
+def nm_liste(m, n, memo=None):
+    if memo is None:
+        # Initialisation d'une matrice (m+1) x (n+1) remplie de None
+        memo = [[None for _ in range(n+1)] for _ in range(m+1)]
+    # Cas de base
+    if m == 1 or n == 1:
+        memo[m][n] = 1
+        return 1
+    # Vérification de la mémoïsation
+    if memo[m][n] is not None:
+        return memo[m][n]
+    # Récursion
+    memo[m][n] = nm_liste(m-1, n, memo) + nm_liste(m, n-1, memo)
+    return memo[m][n]
+
+
+
 # avec functools.cache
 from functools import cache
 @cache
@@ -37,12 +55,13 @@ def nc(m, n):
 # Mesure des durées d'exécution
 import time
 
-for f in (nr, nm, nc):
+for f in nr, nm, nm_liste, nc:
     start = time.time()
-    result = f(10,20)
+    result = f(12, 15)
     end = time.time()
     print(f'Résultat : {result}')
-    print(f'Durée : {end-start}')
+    print(f'Durée : {(end-start)*1e6:.2f} μs')
     print()
+
 
 
